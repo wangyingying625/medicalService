31b0e9329c480975f36d096ef22a52043639f35a
meng <1224374496@qq.com>
2019-04-08 20:46:42 +0800

    add history

---
+++ resources/views/indicator/history.blade.php
@@ -0,0 +1,22 @@
+@extends('indicator.head')
+@section('record')
+    <!-- End Navbar -->
+    <div class="panel-header panel-header-sm">
+    </div>
+    <div class="content">
+        <div class="row">
+            <div class="col-md-8 ml-auto mr-auto">
+                <div class="card card-upgrade">
+                    <div class="card-header text-center">
+                        <h4 class="card-title">上传历史</h4>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+@endsection
