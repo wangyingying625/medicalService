d8416a5b59d61c9a4e3a100454d3711f9a0da941
meng <1224374496@qq.com>
2019-04-10 09:43:21 +0800

    hack for test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <title>健康随身册</title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <link rel="shortcut icon" href="{{ asset('img/logo.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}"/>
     <noscript>
         <link rel="stylesheet" href="{{ asset('css/noscript.css') }}"/>
