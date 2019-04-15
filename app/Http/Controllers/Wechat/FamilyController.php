0298255bcbd3470223421d6a7ad530790b3900ad
taro <180018>
2019-04-15 12:15:00 +0800

    fix family

--- app/Http/Controllers/Wechat/FamilyController.php
+++ app/Http/Controllers/Wechat/FamilyController.php
@@ -31,7 +31,7 @@ class FamilyController extends Controller
         $FamilyId = $request->input('family_id');
         $openId = $request->input('openId');
         $family = Family::find($FamilyId);
-        $members = User::where('family_id',$family->id)->get();
+        $members = User::where('family_id',$family->id)->where('status','member')->get();
         foreach ($members as $member){
             $birthday = $member->birthday;
             $birthday = new DateTime($birthday);
