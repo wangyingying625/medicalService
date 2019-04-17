c835c1494918c90fae9d48817d0055d21ba8cbc4
mldwyy <1224374496@qq.com>
2019-04-17 16:04:06 +0800

    fix scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        \URL::forceScheme('https');
+
+        \URL::forceScheme(env('SCHEME', 'https'));
     }
 
     /**
